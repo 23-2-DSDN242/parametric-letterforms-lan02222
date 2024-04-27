cb125d6ede7419512c1069b48382567ba5392056
lan02222 <161381436+lan02222@users.noreply.github.com>
2024-04-27 23:46:56 +1200

    Update draw_letters.js

--- draw_letters.js
+++ draw_letters.js
@@ -16,27 +16,61 @@ const strokeColor  = "#0a2d27";
  * from (0,0) to (100, 200)
  */
 function drawLetter(letterData) {
-  let size2 = letterData["size"];
-  let lightGreenX = 0 + letterData["offsetx"];
-  let lightGreenY = 0 + letterData["offsety"];
-  let colorVariablility = letterData["lerpColorAmt"]
-  let lineChange = letterData["lineHeight"]
+  let rectw = letterData["wsize"];
+  let recth = letterData["hsize"];
+  let x1 = 0 + letterData["offsetx1"];
+  let y1 = 0 + letterData["offsety1"];
+  let x2 = 0 + letterData["offsetx2"];
+  let y2 = 0 + letterData["offsety2"];
+  let x3 = 0 + letterData["offsetx3"];
+  let y3 = 0 + letterData["offsety3"];
+  let x4 = 0 + letterData["offsetx4"];
+  let y4 = 0 + letterData["offsety4"];
+  let roangle1 = 0 + letterData["roangle1"];
+  // let colorVariablility = letterData["lerpColorAmt"]
+  // let lineChange = letterData["lineHeight"]
 
-  let myGreen = color("#30dfc4")
-  let myPurple = color("#9b3fbf")
+  // let myGreen = color("#30dfc4")
+  // let myPurple = color("#9b3fbf")
 
-  let fillSecondary = lerpColor(myGreen,myPurple,colorVariablility)
+  // let fillSecondary = lerpColor(myGreen,myPurple,colorVariablility)
+
+  
+  // draw a line and a rect
+
+  // angleMode(DEGREES);
+  // rectMode(CENTER);
+  
+  fill(darkGreen)
+  // push()
+  // translate(lightGreenX, lightGreenY)
+  // rotate(roangle1);
+  rect(30, 20, rectw, recth); 
+  
+  
+  strokeWeight(4);
+  strokeCap(PROJECT);
+  noFill();
+  
+  beginShape();
+  vertex(x1,y1); //30, 20
+  vertex(x2, y2);
+  vertex(x3, y3);
+  vertex(x4, y4);
+
+  endShape();
+
 
   
 
   // draw two circles
-  fill(darkGreen);
-  ellipse(0, 0, 50, 50);
+  // fill(darkGreen);
+  // ellipse(0, 0, 50, 50);
 
-  fill(fillSecondary);
-  ellipse(lightGreenX, lightGreenY, size2, size2);
+  // fill(fillSecondary);
+  // ellipse(lightGreenX, lightGreenY, size2, size2);
   
-  line(0-150/2,0,0-150/2,0-lineChange)
+  // line(0-150/2,0,0-150/2,0-lineChange)
 }
 
 
